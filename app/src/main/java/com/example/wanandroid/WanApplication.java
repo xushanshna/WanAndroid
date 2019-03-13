86ebe8c3baed8848063d52a37c343c75c512eb36
xushanshna <xushanshan9592@163.com>
2019-03-13 10:11:24 +0800

    引入依赖

---
+++ app/src/main/java/com/example/wanandroid/WanApplication.java
@@ -0,0 +1,41 @@
+package com.example.wanandroid;
+
+import android.app.Application;
+import android.support.annotation.Nullable;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
+/**
+ * Created by Administrator on 2019/3/13 0013.
+ */
+
+public class WanApplication extends Application {
+    private static final boolean DEBUG = true;
+    private static final String TAG = "WanApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initLogger();
+    }
+
+    private void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+                .methodCount(0)         // (Optional) How many method line to show. Default 2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+//                .logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
+                .tag(TAG)   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build();
+
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            @Override
+            public boolean isLoggable(int priority, @Nullable String tag) {
+                return DEBUG;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/wanandroid/utils/DensityUtil.java
@@ -0,0 +1,34 @@
+package com.example.wanandroid.utils;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2019/3/13 0013.
+ * dp与px、sp互换
+ */
+
+public class DensityUtil {
+
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
